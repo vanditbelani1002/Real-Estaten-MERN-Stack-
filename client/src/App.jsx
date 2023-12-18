183d939f7596d5b8dccbb36f721aa35e9f67f491
vanditbelani1002 <vanditbelani.2003@gmail.com>
2023-12-18 17:24:20 +0530

    Created CreateListing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Profile from './pages/Profile'
 import Header from './components/Header'
+import CreateListing from './pages/CreateListing'
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/about' element={<About />} />
       <Route path='/profile' element={<Profile />} />
+      <Route path='/create-listing' element={<CreateListing />} />
     </Routes>
     </BrowserRouter>
   )
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Profile() {
   return (
@@ -10,6 +11,7 @@ export default function Profile() {
             <input type="email" placeholder='Email' className='border p-3 rounded-lg' id='email' />
             <input type="password" placeholder='Password' className='border p-3 rounded-lg' id='password' />
             <button className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>update</button>
+            <Link to={'/create-listing'} className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95'>Create Listing</Link>
         </form>
         <div className='flex justify-between mt-5'>
         <span className='text-red-700 cursor-pointer'>Delete Account</span>
